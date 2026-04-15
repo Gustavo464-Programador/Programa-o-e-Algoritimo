0ad6ac0a7f2a9cfe1987545b155bce5f29de4bd0
Gustavo464-Programador <gmartinsdatrindade@gmail.com>
2026-04-15 12:31:27 -0300

    Add files via upload

---
+++ Aula 5/Atividades do Dia 15/Exercicio1/src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args)  {
+
+
+        int valor = 10;
+                System.out.println("Valor" + valor);
+
+
+
+
+    }
+}
+
+
+
+
+
---
+++ Aula 5/Atividades do Dia 15/Exercicio2/src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int a;
+        a = 6;
+        int b;
+        b = 2;
+        int c;
+        c = 3;
+
+        System.out.println("Valor A" + a);
+        System.out.println("valor b" + b);
+        System.out.println("Valor c" + c);
+
+
+
+
+
+    }
+}
+
