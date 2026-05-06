c381b7bc16de5ef12eca8c8b2b7a988853a7ed48
Gustavo464-Programador <gmartinsdatrindade@gmail.com>
2026-05-06 09:56:58 -0300

    Add files via upload

---
+++ Aula 6/Atividade4/src/Calculo da Mioridade.java
@@ -0,0 +1,19 @@
+
+import javax.swing.JOptionPane;
+public class Calculo da Mioridade{
+
+    public static void main{
+
+
+        int idade;
+        int data_N;
+        int data_A;
+
+        int idade = Integer.parseInt(null,"Digite sua data de nascimento");
+        int datas
+
+
+
+    }
+
+        }
