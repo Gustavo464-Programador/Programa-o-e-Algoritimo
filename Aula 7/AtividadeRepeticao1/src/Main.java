f4ba776b226e694b3c2e45596d88ecc7dfd67260
Gustavo464-Programador <gmartinsdatrindade@gmail.com>
2026-05-20 12:15:11 -0300

    Add files via upload

---
+++ Aula 7/AtividadeRepeticao1/src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        for (int i= 0; i < 21; i++) {
+            int resto = i % 2;
+            if (resto == 0) {
+                System.out.println("Numero par" + i);
+            }
+            
+
+        }
+    }
+    }
