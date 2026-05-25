6a350f78be9a229a4df945abcf274e0b54385c25
Gustavo464-Programador <gmartinsdatrindade@gmail.com>
2026-05-25 20:39:27 -0300

    Add files via upload

---
+++ Aula 7/ExercicioRepeticao3/src/ExercicioRepeticao3.java
@@ -0,0 +1,13 @@
+
+public class ExercicioRepeticao3 {
+
+    public static void main(String[] args){
+
+        for(int i = 101; i  < 125; i ++ ) {
+
+            System.out.println("Linha " + i);
+        }
+
+    }
+
+}
