14aa0c5bec3dd9771c1421c9f7ae06f511e7830f
Gustavo464-Programador <gmartinsdatrindade@gmail.com>
2026-05-26 22:39:51 -0300

    Add files via upload

---
+++ Aula 7/ExercicioRepeticao1/src/ExercicioRepeticao1.java
@@ -0,0 +1,13 @@
+public class ExercicioRepeticao1 {
+
+public static void main(String[] args){
+
+    //Criação do laço for
+    for(int i = 1; i < 20; i++){
+
+        //saida de dados via terminal
+        System.out.println("Linha " + i++);
+    }
+}
+
+}
