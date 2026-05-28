6257f17adafd4ebbb86ab74a2ee1f3fa62f8b629
Gustavo464-Programador <gmartinsdatrindade@gmail.com>
2026-05-27 21:04:58 -0300

    Update ExercicioRepeticao1.java

--- Aula 7/ExercicioRepeticao1/src/ExercicioRepeticao1.java
+++ Aula 7/ExercicioRepeticao1/src/ExercicioRepeticao1.java
@@ -5,9 +5,13 @@ public static void main(String[] args){
     //Criação do laço for
     for(int i = 1; i < 20; i++){
 
+        int resto = i % 2;
+      //Criação de um If para saber se o numero é par ou não.
+      if( resto == 0){   
         //saida de dados via terminal
-        System.out.println("Linha " + i++);
+        System.out.println("numeros pares: " + i++);
+      }
     }
-}
+ }
 
 }
